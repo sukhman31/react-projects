acb38d0781c67cc8a162a5bd6b38f7cea14d7ca4
sukhman31 <ssingh8_be20@thapar.edu>
2024-01-03 20:24:26 +0530

    initial commit

---
+++ todo/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
